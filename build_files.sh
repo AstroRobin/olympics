e31bbe774f5d001f468158be5108490734b860cd
Robin Cook <RobinCook9012@gmail.com>
2024-10-12 10:30:37 +0800

    New build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Exit immediately if any command fails
-set -e
+#set -e
 
 # Install project dependencies from the requirements.txt file
-pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Run Django management commands (e.g., migrations and collectstatic)
-python3.12 manage.py migrate --noinput  # Apply database migrations
-python3.12 manage.py collectstatic --noinput  # Collect static files
+echo "Migrating over database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput  # Apply database migrations
+
+echo "Collect static files..."
+python3 manage.py collectstatic --noinput  # Collect static files
