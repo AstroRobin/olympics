712900f993a6f2b4020e27195deef8c7d65dd322
Robin Cook <RobinCook9012@gmail.com>
2024-10-12 10:36:03 +0800

    Stripping back build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,12 +4,12 @@
 #set -e
 
 # Install project dependencies from the requirements.txt file
-python3 -m pip install -r requirements.txt
+#python3 -m pip install -r requirements.txt
 
 # Run Django management commands (e.g., migrations and collectstatic)
-echo "Migrating over database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput  # Apply database migrations
+#echo "Migrating over database..."
+#python3 manage.py makemigrations --noinput
+#python3 manage.py migrate --noinput  # Apply database migrations
 
 echo "Collect static files..."
 python3 manage.py collectstatic --noinput  # Collect static files
