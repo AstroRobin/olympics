1edd1b7a35e45b641244f084107eedfeb9fc9fda
Robin Cook <RobinCook9012@gmail.com>
2024-10-12 10:40:29 +0800

    Adding and apt-get for sqlite3

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,8 @@
 
 # Exit immediately if any command fails
 #set -e
+# Install SQLite development libraries for Python
+apt-get update && apt-get install -y sqlite3 libsqlite3-dev
 
 # Install project dependencies from the requirements.txt file
 python3 -m pip install -r requirements.txt
