b3a3090cf64e2e4413e258e6e88ec06024e57216
Robin Cook <RobinCook9012@gmail.com>
2024-10-12 10:26:27 +0800

    Testing pip3 and python3.12 in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Install project dependencies from the requirements.txt file
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Run Django management commands (e.g., migrations and collectstatic)
-python manage.py migrate --noinput  # Apply database migrations
-python manage.py collectstatic --noinput  # Collect static files
+python3.12 manage.py migrate --noinput  # Apply database migrations
+python3.12 manage.py collectstatic --noinput  # Collect static files
